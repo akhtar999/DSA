4cc58efdf74a94d37df9b409ad97ddd62e911fac
Akhtar <iaktr999@gmail.com>
2023-10-25 22:37:00 +0530

    O(n)

---
+++ BigO-O(n).js
@@ -0,0 +1,12 @@
+function logItem(n){
+    for(let i=0 ; i<n; i++){
+        console.log(i)
+    }
+
+}
+
+logItem(4);
+
+//So, this is an O of n operation O(n).
+//So, we pass the function, the number n and this ran n times
+//the number of operations is going to be proportional to whatever n is.
