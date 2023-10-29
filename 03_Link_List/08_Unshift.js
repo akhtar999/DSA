8816673df844de5005ffe88954be2eeb91d85bd4
Akhtar <iaktr999@gmail.com>
2023-10-29 23:14:37 +0530

    unshift

---
+++ 03_Link_List/08_Unshift.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value) {
+    this.value = value; // (head) 4--> null (tail)
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
